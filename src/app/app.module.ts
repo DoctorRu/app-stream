b2a16e2b072075d326314d08b02748e4ca03c9cc
DoctorRu <vjblan@gmail.com>
2018-04-02 01:10:54 -0300

    Intro page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,40 +1,44 @@
-import { NgModule, ErrorHandler } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
-import { MyApp } from './app.component';
+import {NgModule, ErrorHandler} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {IonicApp, IonicModule, IonicErrorHandler} from 'ionic-angular';
+import {MyApp} from './app.component';
 
-import { MusicPage } from '../pages/music/music';
-import { NewsPage } from '../pages/news/news';
-import { VideoPage } from '../pages/video/video';
-import { TabsPage } from '../pages/tabs/tabs';
+import {MusicPage} from '../pages/music/music';
+import {NewsPage} from '../pages/news/news';
+import {VideoPage} from '../pages/video/video';
+import {TabsPage} from '../pages/tabs/tabs';
 
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
+import {StatusBar} from '@ionic-native/status-bar';
+import {SplashScreen} from '@ionic-native/splash-screen';
+import {IntroPage} from "../pages/intro/intro";
 
 @NgModule({
-  declarations: [
-    MyApp,
-    MusicPage,
-    NewsPage,
-    VideoPage,
-    TabsPage
-  ],
-  imports: [
-    BrowserModule,
-    IonicModule.forRoot(MyApp)
-  ],
-  bootstrap: [IonicApp],
-  entryComponents: [
-    MyApp,
-    MusicPage,
-    NewsPage,
-    VideoPage,
-    TabsPage
-  ],
-  providers: [
-    StatusBar,
-    SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
-  ]
+    declarations: [
+        MyApp,
+        MusicPage,
+        NewsPage,
+        VideoPage,
+        TabsPage,
+        IntroPage
+    ],
+    imports: [
+        BrowserModule,
+        IonicModule.forRoot(MyApp)
+    ],
+    bootstrap: [ IonicApp ],
+    entryComponents: [
+        MyApp,
+        MusicPage,
+        NewsPage,
+        VideoPage,
+        TabsPage,
+        IntroPage
+    ],
+    providers: [
+        StatusBar,
+        SplashScreen,
+        {provide: ErrorHandler, useClass: IonicErrorHandler}
+    ]
 })
-export class AppModule {}
+export class AppModule {
+}
